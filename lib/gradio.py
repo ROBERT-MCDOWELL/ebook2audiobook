5d02423b06227f68a3f2a658c42dccd51e8e77c5
unknown <admin@radsl.net>
2026-09-07 19:00:09 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -457,7 +457,7 @@ header_css = '''
         #gr_row_voice_player, #gr_row_custom_model_list, #gr_row_session, #gr_row_audiobook_list {
             height: 60px !important;
         }
-        #gr_audiobook_player :is(.volume, .empty, .source-selection, .control-wrapper, .settings-wrapper, label), #gr_audiobook_files label[data-testid="block-label"] {
+        #gr_audiobook_player :is(.share, .download, .volume, .empty, .source-selection, .control-wrapper, .settings-wrapper, label), #gr_audiobook_files label[data-testid="block-label"] {
             display: none !important;
         }
         #gr_audiobook_player audio {
@@ -467,6 +467,7 @@ header_css = '''
             border-radius: 0px !important;
             background-color: #ebedf0 !important;
             color: #ffffff !important;
+            border_style: none !important;
         }
         #gr_audiobook_player audio::-webkit-media-controls-panel {
             width: 100% !important;
