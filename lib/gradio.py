92c08734ed2fdacd7928e4128784fe7ccbb5efa0
unknown <admin@radsl.net>
2026-09-08 07:48:16 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -895,7 +895,7 @@ def build_interface(args:dict)->gr.Blocks:
                             gr_abs_url = gr.Textbox(label='Server URL', elem_id='gr_abs_url', value=default_abs_url, placeholder='http://localhost:13378', lines=1, max_lines=1, interactive=True, scale=2)
                             gr_abs_api_token = gr.Textbox(label='API Token', elem_id='gr_abs_api_token', value=default_abs_api_token, type='password', placeholder='eyJ...', lines=1, max_lines=1, interactive=True, scale=1) 
                         with gr.Row(elem_id='gr_row2_abs'):
-                            gr_abs_library = gr.Dropdown(label='', elem_id='gr_abs_library', choices=[], info="Enter URL + API Token to load libraries", value=default_abs_library or None, interactive=True)
+                            gr_abs_library = gr.Dropdown(label='', elem_id='gr_abs_library', choices=[], value=default_abs_library or None, interactive=True)
                             gr_abs_search_btn = gr.Button('🔍', elem_id='gr_abs_search_btn', elem_classes=['gr-abs-search-btn'], variant='', visible=True, interactive=True, scale=0, min_width=60)
                         with gr.Group(elem_id='gr_group_abs_upload_btn', elem_classes=['gr-group-abs-upload-btn']):
                             gr_abs_audiobook = gr.Textbox(elem_id='gr_abs_audiobook', label='Audiobook', lines=1, max_lines=1, interactive=False, visible=True)
