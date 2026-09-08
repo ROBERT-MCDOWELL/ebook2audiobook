e9abe64a4685b3309f9afdfe3a1d9fc1e003e3bb
unknown <admin@radsl.net>
2026-09-08 07:53:34 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1423,7 +1423,7 @@ def build_interface(args:dict)->gr.Blocks:
                         if session.get('audiobook') != selected:
                             session['audiobook'] = selected
                         visible = session['audiobook'] is not None
-                        audiobook_file = selected if selected else ''
+                        audiobook_file = Path(selected).name if selected else ''
                         return gr.update(visible=visible), gr.update(value=audiobook_file)
                 except Exception as e:
                     error = f'_change_gr_audiobook_list(): {e}'
